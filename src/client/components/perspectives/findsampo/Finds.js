564c18a82e3b89bfb1f7faca386804c9ff058fb4
esikkala <esko.ikkala@aalto.fi>
2020-12-07 10:11:22 +0200

    Update map default zoom

--- src/client/components/perspectives/findsampo/Finds.js
+++ src/client/components/perspectives/findsampo/Finds.js
@@ -51,8 +51,10 @@ const Finds = props => {
         path={`${rootUrl}/${perspective.id}/faceted-search/map`}
         render={() =>
           <LeafletMap
-            center={[22.43, 10.37]}
-            zoom={2}
+            // center={[22.43, 10.37]}
+            // zoom={2}
+            center={[65.184809, 27.314050]}
+            zoom={5}
             // center={[60.17, 24.81]}
             // zoom={14}
             results={props.placesResults.results}
