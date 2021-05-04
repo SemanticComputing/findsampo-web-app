45376843db919eeb713290ee5ca9483381ccf6f1
esikkala <esko.ikkala@aalto.fi>
2021-05-04 09:29:20 +0300

    Hide ceramic style facet

--- src/client/reducers/findsampo/findsFacets.js
+++ src/client/reducers/findsampo/findsFacets.js
@@ -113,24 +113,24 @@ export const INITIAL_STATE = {
       uriFilter: null,
       type: 'hierarchical'
     },
-    ceramicStyle: {
-      id: 'ceramicStyle',
-      // predicate: defined in backend
-      distinctValueCount: 0,
-      values: [],
-      flatValues: [],
-      sortBy: 'instanceCount',
-      sortDirection: 'desc',
-      sortButton: false,
-      spatialFilterButton: false,
-      pieChartButton: false,
-      isFetching: false,
-      searchField: true,
-      containerClass: 'ten',
-      filterType: 'uriFilter',
-      uriFilter: null,
-      type: 'hierarchical'
-    },
+    // ceramicStyle: {
+    //   id: 'ceramicStyle',
+    //   // predicate: defined in backend
+    //   distinctValueCount: 0,
+    //   values: [],
+    //   flatValues: [],
+    //   sortBy: 'instanceCount',
+    //   sortDirection: 'desc',
+    //   sortButton: false,
+    //   spatialFilterButton: false,
+    //   pieChartButton: false,
+    //   isFetching: false,
+    //   searchField: true,
+    //   containerClass: 'ten',
+    //   filterType: 'uriFilter',
+    //   uriFilter: null,
+    //   type: 'hierarchical'
+    // },
     length: {
       id: 'length',
       // predicate: defined in backend
