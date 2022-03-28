d01fb7b9f4219c7aee3133bbd6113bbf32e84714
esikkala <esko.ikkala@aalto.fi>
2022-03-28 11:03:53 +0300

    Restore NLS map layers

--- src/client/components/facet_results/LeafletMap.js
+++ src/client/components/facet_results/LeafletMap.js
@@ -276,24 +276,23 @@ class LeafletMap extends React.Component {
       Base layers from https://www.maanmittauslaitos.fi/karttakuvapalvelu/tekninen-kuvaus-wmts
       Routed via backend.
     */
-    // const backgroundMapNLS = L.tileLayer(`${process.env.API_URL}/nls-wmts?z={z}&x={x}&y={y}&layerID=taustakartta`, {
-    //   attribution: 'National Land Survey of Finland',
-    //   maxZoom: 18
-    // })
+    const backgroundMapNLS = L.tileLayer(`${process.env.API_URL}/nls-wmts?z={z}&x={x}&y={y}&layerID=taustakartta`, {
+      attribution: 'National Land Survey of Finland',
+      maxZoom: 18
+    })
     // https://github.com/mapbox/mapbox-gl-leaflet
     // const nlsVectortilesBackgroundmap = L.mapboxGL({
     //   accessToken: this.props.mapBoxAccessToken,
     //   style: `${process.env.API_URL}/nls-vectortiles-open`
     // })
-    // const topographicalMapNLS = L.tileLayer(`${process.env.API_URL}/nls-wmts-open?z={z}&x={x}&y={y}&layerID=maastokartta`, {
-    //   attribution: 'National Land Survey of Finland',
-    //   maxZoom: 18
-    // })
-    // const airMapNLS = L.tileLayer(`${process.env.API_URL}/nls-wmts-open?z={z}&x={x}&y={y}&layerID=ortokuva`, {
-    //   attribution: 'National Land Survey of Finland',
-    //   maxZoom: 18
-    // })
-
+    const topographicalMapNLS = L.tileLayer(`${process.env.API_URL}/nls-wmts?z={z}&x={x}&y={y}&layerID=maastokartta`, {
+      attribution: 'National Land Survey of Finland',
+      maxZoom: 18
+    })
+    const airMapNLS = L.tileLayer(`${process.env.API_URL}/nls-wmts?z={z}&x={x}&y={y}&layerID=ortokuva`, {
+      attribution: 'National Land Survey of Finland',
+      maxZoom: 18
+    })
     // const googleRoadmap = L.gridLayer.googleMutant({
     //   type: 'roadmap'
     // })
@@ -341,10 +340,10 @@ class LeafletMap extends React.Component {
     // initialize layers from external sources
     if (this.props.showExternalLayers) {
       const basemaps = {
-        [intl.get(`leafletMap.basemaps.mapbox.${mapboxStyle}`)]: mapboxBaseLayer
-        // [intl.get('leafletMap.basemaps.backgroundMapNLS')]: nlsVectortilesBackgroundmap,
-        // [intl.get('leafletMap.basemaps.topographicalMapNLS')]: topographicalMapNLS,
-        // [intl.get('leafletMap.basemaps.airMapNLS')]: airMapNLS
+        [intl.get(`leafletMap.basemaps.mapbox.${mapboxStyle}`)]: mapboxBaseLayer,
+        [intl.get('leafletMap.basemaps.backgroundMapNLS')]: backgroundMapNLS,
+        [intl.get('leafletMap.basemaps.topographicalMapNLS')]: topographicalMapNLS,
+        [intl.get('leafletMap.basemaps.airMapNLS')]: airMapNLS
         // [intl.get('leafletMap.basemaps.googleRoadmap')]: googleRoadmap,
       }
       this.initOverLays(basemaps)
